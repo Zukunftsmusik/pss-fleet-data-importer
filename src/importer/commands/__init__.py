3635c146d66a6d820d639cc21b4ef154bbbbb16d
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-25 01:28:46 +0200

    Remove commands folder

--- src/importer/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import cmd_credentials, cmd_download, cmd_import, options
-
-
-__all__ = [
-    cmd_credentials.__name__,
-    cmd_download.__name__,
-    cmd_import.__name__,
-    options.__name__,
-]
