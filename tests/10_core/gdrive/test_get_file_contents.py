b21071a9769f41103c069329f16e3e638b1292af
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-08 18:39:00 +0200

    Add more test cases

--- tests/10_core/gdrive/test_get_file_contents.py
+++ tests/10_core/gdrive/test_get_file_contents.py
@@ -13,8 +13,18 @@ class MockResponse:
     status: int = 400
 
 
+test_cases_do_not_log_exception = [
+    # exception_type: type[Exception]
+    pytest.param(Exception, id="exception"),
+    pytest.param(IOError, id="io_error"),
+    pytest.param(ValueError, id="value_error"),
+    pytest.param(TypeError, id="type_error"),
+]
+"""exception_type: type[Exception]"""
+
+
 test_cases_log_exception = [
-    # exception_type, exception_instance
+    # exception_type: type[Exception], exception_instance: Exception
     pytest.param(
         pydrive2.files.ApiRequestError,
         pydrive2.files.ApiRequestError(googleapiclient.errors.HttpError(MockResponse(), b"{}")),
@@ -22,21 +32,42 @@ test_cases_log_exception = [
     ),
     pytest.param(pydrive2.files.FileNotDownloadableError, pydrive2.files.FileNotDownloadableError(), id="file_not_downloadable_error"),
 ]
-"""exception_type, exception_instance"""
+"""exception_type: type[Exception], exception_instance: Exception"""
+
 
 test_cases_valid = [
-    # content
+    # content: Optional[str]
     pytest.param(None, id="none"),
     pytest.param("", id="str_empty"),
     pytest.param("content", id="str_content"),
     pytest.param("{}", id="json_empty"),
     pytest.param("null", id="json_null"),
 ]
-"""content"""
+"""content: Optional[str]"""
+
+
+@pytest.mark.parametrize(["exception_type"], test_cases_do_not_log_exception)
+def test_get_file_contents_do_not_log_exception(
+    exception_type: type[Exception],
+    monkeypatch: pytest.MonkeyPatch,
+    caplog: pytest.LogCaptureFixture,
+):
+    def mock_GetContentString(*args):
+        raise exception_type()
+
+    monkeypatch.setattr(pydrive2.files.GoogleDriveFile, pydrive2.files.GoogleDriveFile.GetContentString.__name__, mock_GetContentString)
+
+    file: pydrive2.files.GoogleDriveFile = pydrive2.files.GoogleDriveFile(None)
+
+    with caplog.at_level(logging.WARNING):
+        with pytest.raises(exception_type):
+            _ = get_file_contents(file, logging.getLogger())
+
+    assert "An error occured while downloading file" not in caplog.text
 
 
 @pytest.mark.parametrize(["exception_type", "exception_instance"], test_cases_log_exception)
-def test_get_file_contents_invalid(
+def test_get_file_contents_log_exception(
     exception_type: type[Exception],
     exception_instance: Exception,
     monkeypatch: pytest.MonkeyPatch,
