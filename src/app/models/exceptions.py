1d57182e24582445eb6009faa83b4a47315dc613
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-09 10:29:37 +0200

    Add inner_exception parameter

--- src/app/models/exceptions.py
+++ src/app/models/exceptions.py
@@ -1,15 +1,19 @@
+from typing import Optional
+
+
 class DownloadFailedError(Exception):
-    def __init__(self, file_name: str, reason: str):
-        self.file_name = file_name
-        self.reason = reason
+    def __init__(self, file_name: str, reason: str, inner_exception: Optional[Exception] = None):
+        self.file_name: str = file_name
+        self.inner_exception: Optional[Exception] = inner_exception
         self.message = f"Download of file '{file_name}' failed: {reason}"
+        self.reason: str = reason
         super().__init__(self.message)
 
     def __str__(self) -> str:
         return self.message
 
     def __repr__(self) -> str:
-        return f"<{DownloadFailedError.__name__} file_name={self.file_name}, reason={self.reason}>"
+        return f"<{DownloadFailedError.__name__} file_name={self.file_name}, reason={self.reason}, inner_exception={type(self.inner_exception)}>"
 
 
 __all__ = [
