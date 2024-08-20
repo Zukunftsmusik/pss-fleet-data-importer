6ec24f17c0a68c5c1bba235180f7d11ddaca0547
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-20 16:02:08 +0200

    Restore original loop call

--- src/app/main.py
+++ src/app/main.py
@@ -58,8 +58,10 @@ async def main():
     if await importer.check_api_server_connection():
         try:
             # await importer.run_import_loop(modified_after=datetime(2024, 8, 16, 12), modified_before=datetime(2024, 8, 16, 13))
-            await importer.run_import_loop(run_once=True, modified_before=datetime(2019, 10, 20))
-            # await importer.run_import_loop()
+            # await importer.run_import_loop(modified_after=datetime(2024, 8, 20, 10))
+            # await importer.run_import_loop(run_once=True, modified_before=datetime(2022, 4, 1))  # Including 2 months of schema version 9
+            # await importer.run_import_loop(run_once=True, modified_before=datetime(2019, 10, 20))
+            await importer.run_import_loop()
         except KeyboardInterrupt:
             logger_base.aborted()
             importer.cancel_workers()
