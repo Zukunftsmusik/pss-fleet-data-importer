6a40d7a6159781dd31c4473ff92285f8fdd23178
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-20 16:17:41 +0200

    Remove test methods

--- tests/unit/10_core/config/test_config_properties.py
+++ tests/unit/10_core/config/test_config_properties.py
@@ -7,25 +7,13 @@ import pytest
 from tests.fake_classes import FakeConfig
 
 
-def test_async_connection_string_well_formed(mock_config: FakeConfig):
-    assert mock_config.db_async_connection_str.startswith("postgresql+asyncpg://")
-    assert mock_config.db_url in mock_config.db_async_connection_str
-    assert mock_config.db_name in mock_config.db_async_connection_str
+def test_db_server_and_port_correct(fake_config: FakeConfig):
+    fake_config.db_url = "user:password@127.0.0.1:8000"
+    assert fake_config.db_server_and_port == "127.0.0.1:8000"
 
 
-def test_sync_connection_string_well_formed(mock_config: FakeConfig):
-    assert mock_config.db_sync_connection_str.startswith("postgresql://")
-    assert mock_config.db_url in mock_config.db_sync_connection_str
-    assert mock_config.db_name in mock_config.db_sync_connection_str
-
-
-def test_db_server_and_port_correct(mock_config: FakeConfig):
-    mock_config.db_url = "user:password@127.0.0.1:8000"
-    assert mock_config.db_server_and_port == "127.0.0.1:8000"
-
-
-def test_log_file_name_well_formed(mock_config: FakeConfig):
-    log_file_name = mock_config.log_file_name
+def test_log_file_name_well_formed(fake_config: FakeConfig):
+    log_file_name = fake_config.log_file_name
 
     assert isinstance(log_file_name, str)
     assert log_file_name.startswith("pss_fleet_data_importer_")
@@ -33,21 +21,21 @@ def test_log_file_name_well_formed(mock_config: FakeConfig):
     assert len(log_file_name) == len("pss_fleet_data_importer_") + len(".log") + len("20240101-235900")
 
 
-def test_log_folder_path_is_set(mock_config: FakeConfig):
+def test_log_folder_path_is_set(fake_config: FakeConfig):
     mock_path = "/dev/null"
-    mock_config.log_folder = mock_path
+    fake_config.log_folder = mock_path
 
-    assert isinstance(mock_config.log_folder_path, Path)
-    assert str(mock_config.log_folder_path) == mock_path
-    assert isinstance(mock_config.log_file_path, Path)
-    assert len(str(mock_config.log_file_path)) == len(mock_path) + 1 + len("pss_fleet_data_importer_") + len(".log") + len("20240101-235900")
+    assert isinstance(fake_config.log_folder_path, Path)
+    assert str(fake_config.log_folder_path) == mock_path
+    assert isinstance(fake_config.log_file_path, Path)
+    assert len(str(fake_config.log_file_path)) == len(mock_path) + 1 + len("pss_fleet_data_importer_") + len(".log") + len("20240101-235900")
 
 
-def test_log_folder_path_not_set(mock_config: FakeConfig):
-    mock_config.log_folder = None
+def test_log_folder_path_not_set(fake_config: FakeConfig):
+    fake_config.log_folder = None
 
-    assert mock_config.log_folder_path is None
-    assert mock_config.log_file_path is None
+    assert fake_config.log_folder_path is None
+    assert fake_config.log_file_path is None
 
 
 test_cases_log_level = [
@@ -66,8 +54,8 @@ test_cases_log_level = [
 
 
 @pytest.mark.parametrize(["from_env", "debug_mode", "expected_result"], test_cases_log_level)
-def test_log_level_correct(mock_config: FakeConfig, from_env: Optional[str], debug_mode: bool, expected_result: int):
-    mock_config.log_level = from_env
-    mock_config.debug_mode = debug_mode
+def test_log_level_correct(fake_config: FakeConfig, from_env: Optional[str], debug_mode: bool, expected_result: int):
+    fake_config.log_level = from_env
+    fake_config.debug_mode = debug_mode
 
-    assert mock_config.app_log_level == expected_result
+    assert fake_config.app_log_level == expected_result
